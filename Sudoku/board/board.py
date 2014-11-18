3b947ac5daa937a4def7177d699be076d4c6eca1
Buzurg Arjmandi <zeebenzene@gmail.com>
2014-11-17 20:33:13 -0600

    bugfixes and board changes

--- Sudoku/board/board.py
+++ Sudoku/board/board.py
@@ -7,6 +7,7 @@ import random
 class Board:
     def __init__(self, boardName):
         self.wholeBoard = [[0 for i in range(1,10)] for x in range(9)]
+        # self.wholeBoard = self.openBoard(boardName)
         self.freePool = self.createFreePool()
 
     def createSolvedBoard(self):
@@ -24,9 +25,13 @@ class Board:
         # while there are still free elements
         while(len(self.freePool)> 0):
             location = random.choice(self.freePool)  #get a random free location
+            self.freePool.remove(location)
             row = location[0]
             col = location[1]
             potential = self.getAvaliableInSpace(row,col)  #get the available elements for that location
+            if potential == "filled":
+            	print(location)
+            	self.prettyPrint()
             if(len(potential) ==0):
                 while(len(potential) == 0):
                     if(len(path) ==0):
@@ -34,24 +39,27 @@ class Board:
                         break
                     lastTried = path[-1]
                     path.remove(lastTried)
-                    self.wholeBoard[lastTried[0],lastTried[1]] = 0
                     self.freePool.append(lastTried)
+                    self.wholeBoard[lastTried[0]][lastTried[1]] = 0
                     potential = self.getAvaliableInSpace(lastTried[0],lastTried[1])
                     for ele in tried[lastTried]:
-                        potential.remove(ele)
+                        if ele in potential:
+                            potential.remove(ele)
                     if len(potential) == 0:
                         del tried[lastTried]
+                    else:
+                    	self.freePool.remove(lastTried)
+                    	row = lastTried[0]
+                    	col = lastTried[1]
                     
 
             answer = random.choice(potential)
-            self.wholeBoard[row,col] = answer
+            self.wholeBoard[row][col] = answer
             path.append(location)
-            self.freePool.remove(location)
             if(location not in tried):
                 tried[location] = [answer]
             else:
                 tried[location].append(answer)
-        return self.wholeBoard
 
     def openBoard(self, boardName):
         sep = os.sep
@@ -130,12 +138,15 @@ class Board:
     def getAvailableInList(self, list):
         fullList = [x for x in range(1, 10)]
         return filter(lambda x: x not in list, fullList)
+    def prettyPrint(self):
+        for row in self.wholeBoard:
+            print(row)
 
-board = Board("board1.txt")
-
-print(board.createSolvedBoard())
+board = Board("board2.txt")
+board.createSolvedBoard()
+board.prettyPrint()
 #print(board.getAvailableInSubBoard(0, 0))
 #print(board.getAvailableInRow(0))
 #print(board.getAvaliableInColumn(0))
 #print(board.getElement(2, 0))
-#print(board.getAvaliableInSpace(2, 2))
+#print(board.getAvaliableInSpace(0, 1))
