b5e7f22dad5edc4e9a4097e0f8719ada4ae394c9
Buzurg Arjmandi <zeebenzene@gmail.com>
2014-12-13 16:47:54 -0600

    cleaned up board.  removed createSolvedBoard because it was taking up too much space and making things hard to read.

--- Sudoku/board/board.py
+++ Sudoku/board/board.py
@@ -6,60 +6,12 @@ import random
 
 class Board:
     def __init__(self, boardName):
-        self.wholeBoard = [[0 for i in range(1,10)] for x in range(9)]
-        # self.wholeBoard = self.openBoard(boardName)
+        # self.wholeBoard = self.createBlankBoard()
+        self.wholeBoard = self.openBoard(boardName)
         self.freePool = self.createFreePool()
 
-    def createSolvedBoard(self):
-        # select 9 random locations from the free pool and put numbers 1 - 9 in them
-        for i in range(1,10):
-            location = random.choice(self.freePool)
-            row = location[0]
-            col = location[1]
-            self.wholeBoard[row][col] = i
-            self.freePool.remove(location)
-
-
-        tried = {}
-        path = []
-        # while there are still free elements
-        while(len(self.freePool)> 0):
-            location = random.choice(self.freePool)  #get a random free location
-            self.freePool.remove(location)
-            row = location[0]
-            col = location[1]
-            potential = self.getAvaliableInSpace(row,col)  #get the available elements for that location
-            if potential == "filled":
-            	print(location)
-            	self.prettyPrint()
-            if(len(potential) ==0):
-                while(len(potential) == 0):
-                    if(len(path) ==0):
-                        print("Puzzle could not be created") #This should hopefully never happen
-                        break
-                    lastTried = path[-1]
-                    path.remove(lastTried)
-                    self.freePool.append(lastTried)
-                    self.wholeBoard[lastTried[0]][lastTried[1]] = 0
-                    potential = self.getAvaliableInSpace(lastTried[0],lastTried[1])
-                    for ele in tried[lastTried]:
-                        if ele in potential:
-                            potential.remove(ele)
-                    if len(potential) == 0:
-                        del tried[lastTried]
-                    else:
-                    	self.freePool.remove(lastTried)
-                    	row = lastTried[0]
-                    	col = lastTried[1]
-                    
-
-            answer = random.choice(potential)
-            self.wholeBoard[row][col] = answer
-            path.append(location)
-            if(location not in tried):
-                tried[location] = [answer]
-            else:
-                tried[location].append(answer)
+    def createBlankBoard(self):
+        return [[0 for i in range(1,10)] for x in range(9)]
 
     def openBoard(self, boardName):
         sep = os.sep
@@ -79,24 +31,17 @@ class Board:
                     freePool.append((i,j))
         return freePool
 
-
     def isDuplicateIn(self, list, value):
-        if list.count(value) == 1:
-            return False
-        else:
-            return True
+        return not (list.count(value) == 1)
 
     def getElement(self, row, col):
         return self.wholeBoard[row][col]
-
     def getColumn(self, index):
         return  [row[index] for row in self.wholeBoard]
-
     def getRow(self, index):
         return self.wholeBoard[index]
 
     #starting at 0 - 2
-
     def getSubBoardAsList(self, rows, cols):
         subBoardAsList = []
         for y in range((rows*3), ((rows+1)*3)):
@@ -105,26 +50,20 @@ class Board:
                 subBoardAsList.append(val)
         return subBoardAsList
 
+    def rawToSubIndex(self, idx):
+        return idx / 3
+
     def getAvaliableInSpace(self, row, col):
         if self.getElement(row, col) == 0:
             fullList = [i for i in range(1, 10)]
             rowAvail = self.getAvailableInRow(row)
             colAvail = self.getAvaliableInColumn(col)
-            subAvail = self.getAvailableInSubBoard(self.modIndex(row), self.modIndex(col))
+            subAvail = self.getAvailableInSubBoard(self.rawToSubIndex(row), self.rawToSubIndex(col))
 
             filterLambda = lambda x: (x in rowAvail) and (x in colAvail) and (x in subAvail)
             return filter(filterLambda, fullList)
         else:
             return "filled"
-
-    def modIndex(self, idx):
-        if 0 <= idx <= 2:
-            return 0
-        if 2 < idx <= 5:
-            return 1
-        if 5 < idx <= 8:
-            return 2
-
     def getAvailableInSubBoard(self, row, col):
         list = self.getSubBoardAsList(row, col)
         return self.getAvailableInList(list)
@@ -134,17 +73,18 @@ class Board:
     def getAvaliableInColumn(self, col):
         list = self.getColumn(col)
         return self.getAvailableInList(list)
-
     def getAvailableInList(self, list):
         fullList = [x for x in range(1, 10)]
         return filter(lambda x: x not in list, fullList)
+
+
     def prettyPrint(self):
         for row in self.wholeBoard:
             print(row)
 
-board = Board("board2.txt")
-board.createSolvedBoard()
-board.prettyPrint()
+# board = Board("board2.txt")
+# board.createSolvedBoard()
+# board.prettyPrint()
 #print(board.getAvailableInSubBoard(0, 0))
 #print(board.getAvailableInRow(0))
 #print(board.getAvaliableInColumn(0))
